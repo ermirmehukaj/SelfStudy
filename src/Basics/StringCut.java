b7d4f18ef7dd544c3b0f35cebd0bebe3e2184ec3
Ermir <ermirmehukaj22@gmail.com>
2024-04-14 14:50:02 +0200

    stringCut

---
+++ src/Basics/StringCut.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class StringCut {
+
+    public static void main(String[] args) {
+
+        String str = "Java";
+        String str1 = "Python";
+
+        System.out.println(str.substring(1 ) + str1.substring(1));
+
+
+    }
+}
+/*
+Write a Java program to create the concatenation of the two strings except removing the first character of each string.
+The length of the strings must be 1 and above.
+ */
