7c1e4c3de1d6decc854f7d504eab06dc3fe4482a
Ermir <ermirmehukaj22@gmail.com>
2024-04-14 14:42:54 +0200

    input and display

---
+++ src/Basics/InputAndDisplay.java
@@ -0,0 +1,24 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class InputAndDisplay {
+
+    public static void main(String[] args) {
+
+        System.out.println("Twinkle, twinkle, little star,\n\t How I wonder what you are!\n\t\tUp above the world so high," +
+                "\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are*/\n\n");
+
+    }
+}
+/*
+Write a Java program to print the following string in a specific format (see output).
+
+
+
+Twinkle, twinkle, little star,
+        How I wonder what you are!
+        Up above the world so high,
+        Like a diamond in the sky.
+        Twinkle, twinkle, little star,
+        How I wonder what you are*/
