9d7be61ff610655e16061820a3b9d7c5dc81283f
Ermir <ermirmehukaj22@gmail.com>
2024-04-25 16:12:09 +0200

    Frequency of array elements

--- src/GroupPractice/frequencyOfArrayElement.java
+++ src/GroupPractice/frequencyOfArrayElement.java
@@ -7,28 +7,28 @@ import java.util.Map;
 
 public class frequencyOfArrayElement {
 
-    public static Map<String,Integer> frequencyOfArray(String[] array){
+    public static Map<String, Integer> frequencyOfArray(String[] array) {
 
-        Map<String,Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
 
         for (String each : array) {
             String lowercaseElement = each.toLowerCase();
 
             Integer count = map.get(lowercaseElement);
 
-            if(count ==null){
-                map.put(lowercaseElement,1);
-            }else {
-                map.put(lowercaseElement, count+1);
+            if (count == null) {
+                map.put(lowercaseElement, 1);
+            } else {
+                map.put(lowercaseElement, count + 1);
             }
 
         }
-return map;
+        return map;
     }
 
     public static void main(String[] args) {
 
-        String [] str =  {"Apple", "Banana", "apple", "Cherry", "Apple"};
+        String[] str = {"Apple", "Banana", "apple", "Cherry", "Apple"};
 
         System.out.println(frequencyOfArray(str));
     }
