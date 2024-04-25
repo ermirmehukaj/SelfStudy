829be8d9d9c12f957f616c44d68ffafcb5182de6
Ermir <ermirmehukaj22@gmail.com>
2024-04-25 15:42:50 +0200

    majorityElements

---
+++ src/Basics/MajorityElemnts.java
@@ -0,0 +1,34 @@
+package Basics;
+
+import java.util.Arrays;
+
+public class MajorityElemnts {
+
+
+    public static void main(String[] args) {
+          int [] myArray = {2,2,1,1,1,1,2};
+
+        Arrays.sort(myArray);
+        int n = myArray.length;
+        System.out.println(myArray[n / 2]);
+    }
+}
+/*
+
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+ You may assume that the majority element always exists in the array.
+
+
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ */
