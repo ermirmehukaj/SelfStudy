538126fa2c6ca5020efba326b4f90897068266da
Ermir <ermirmehukaj22@gmail.com>
2024-05-13 00:09:06 +0200

    to lowerCase

---
+++ src/Basics2/ToLowerCase.java
@@ -0,0 +1,16 @@
+package Basics2;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+
+
+        System.out.println(lower("HOME"));
+    }
+
+    public static String lower(String str){
+
+
+
+        return str.toLowerCase();
+    }
+}
