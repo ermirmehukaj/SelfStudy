efce1c4822d1fde1b7aea0d80d9a0e2b62d243e6
Ermir <ermirmehukaj22@gmail.com>
2024-05-29 11:04:17 +0200

    MajorityElement

---
+++ src/Basic3/MajorityElement.java
@@ -0,0 +1,19 @@
+package Basic3;
+
+import java.util.Arrays;
+
+public class MajorityElement {
+
+    public static void main(String[] args) {
+        int [] arr = {2,2,1,1,1,2,2};
+
+        System.out.println(majorityElement(arr));
+    }
+
+    public static int majorityElement(int[] nums) {
+
+
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
