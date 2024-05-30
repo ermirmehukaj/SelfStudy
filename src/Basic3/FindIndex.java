2746e3a7f5a32aeecfde907b34edbbb91ead7690
Ermir <ermirmehukaj22@gmail.com>
2024-05-30 18:21:13 +0200

    FindIndex

---
+++ src/Basic3/FindIndex.java
@@ -0,0 +1,35 @@
+package Basic3;
+
+public class FindIndex {
+    public static void main(String[] args) {
+
+        String myStr = "leetcode";
+        String str1 = "leeto";
+        System.out.println(strStr(myStr, str1));
+    }
+
+
+    public static int strStr(String haystack, String needle) {
+
+        return haystack.indexOf(needle);
+    }
+
+}
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+ or -1 if needle is not part of haystack.
+
+
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ */
