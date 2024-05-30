44cfa2a59bd9b9862f5e37f683130a0ba98cf8a3
Ermir <ermirmehukaj22@gmail.com>
2024-05-31 00:01:05 +0200

    ShortestWord

--- src/Basic3/ShortestWord.java
+++ src/Basic3/ShortestWord.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class ShortestWord {
 
     public static void main(String[] args) {
-        System.out.println(length1("Ermiri osht shume i fort"));
+        System.out.println(length1("Ermiri is here"));
     }
     
     
